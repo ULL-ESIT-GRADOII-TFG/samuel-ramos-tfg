a309cbba2804fb363d3c3c5c3f57bb908ea4f549
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-02-10 12:37:28 +0000

    Configurando el servidor para el uso de mongo

--- app/mongo/user.js
+++ app/mongo/user.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
-const crypto = require('crypto');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
   login: { type: String, unique: true, lowecase: true },
@@ -10,4 +8,6 @@ const UserSchema = new Schema({
   signupDate: { type: Date, default: Date.now() },
   lastLogin: Date
 
-});
+})
+
+module.exports = mongoose.model('User', UserSchema)
--- config/configServer.js
+++ config/configServer.js
@@ -1,3 +1,4 @@
 module.exports = {
-  port: process.env.PORT || 8081
+  port: process.env.PORT || 8081,
+  db: process.env.MONGODB || 'mongodb://localhost/codelab'
 }
