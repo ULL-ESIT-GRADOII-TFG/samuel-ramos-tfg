344f58303002c9478eea0e0f7c957fdcc0c0fa7d
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-02-11 23:35:42 +0000

    Añadiendo el código de Jquery para obtener el id de cualquier a en el que se haga click

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
-  $(".button-collapse").sideNav();
-  $('#test').click(() => {
-    console.log($('#test').attr('id'))
-    $.post('/orgs', { id: $('#test').attr('id') }, (data, status, jqXHR) => {
-         console.log('status: ' + status + ', data: ' + data)
-       })
+  $(".button-collapse").sideNav()
+
+  $("a").click(function() {
+    $.post('/orgs', { id: this.id }, (data, status, jqXHR) => {
+      console.log('status: ' + status + ', data: ' + data)
+    })
   })
 })
