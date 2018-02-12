c4182b76570e8a6476c0dc6e4916fc06d62b2182
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-02-12 00:19:11 +0000

    Modificando el nombre del archivo

--- public/js/getId.js
+++ public/js/getId.js
@@ -2,7 +2,6 @@ $(document).ready(() => {
   $(".button-collapse").sideNav()
 
   $("a.seleccion").click(function() {
-    console.log("hola");
     $.post('/orgs', { id: this.id }, (data, status, jqXHR) => {
       console.log('status: ' + status + ', data: ' + data)
     })
