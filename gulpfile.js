edbced4069c19fc5effb961397d9b934ea26b4cc
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-03-31 16:54:42 +0100

    Modificando links a las invitaciones para heroku

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,30 @@ const nodemon = require('gulp-nodemon')
 
 const dotenv = 'CLIENTID=' + '\nCLIENTSECRET=' + '\nCALLBACKURL=' + '\n\nTOKENFORTEST='
 
+// Tarea para escribir el fichero .env
 gulp.task('default-env', () => {
   fs.writeFileSync('.env', dotenv)
 })
 
+// Tarea para compilar saaas
 gulp.task('sass', () => {
   return gulp.src('./public/custom.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'))
 })
 
+// Tarea para compilar saaas
 gulp.task('sass:watch', () => {
   gulp.watch('./public/custom.scss', ['sass'])
 })
 
+// Taraa para abrir en el navegador
 gulp.task('open', () => {
   gulp.src(__filename)
   .pipe(open({ uri: 'http://localhost:8081' }))
 })
 
+// Tarea para nodemon
 gulp.task('browser-sync', ['nodemon'], () => {
   browserSync.init(null, {
     proxy: 'http://localhost:3000',
