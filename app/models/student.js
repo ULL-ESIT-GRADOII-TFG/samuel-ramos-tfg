9dd9aaf1515a4ed7ed1c156bb5944da0f78600e7
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-04-09 18:43:45 +0100

    Solucionando un error que permitia valores duplicados en la colección alumnos

--- app/models/student.js
+++ app/models/student.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const StudentSchema = new Schema({
-  name: { type: String, unique: true },
+  name: String,
   surname: String,
-  email: String,
+  email: { type: String, index: true, unique: true },
   idGithub: String,
-  orgName: { type: String, unique: true },
+  orgName: String,
   signupDate: { type: Date, default: Date.now() }
 })
-
+StudentSchema.index({ 'email': 1, 'orgNAme': 1 }, { 'unique': true })
 module.exports = mongoose.model('Student', StudentSchema)
