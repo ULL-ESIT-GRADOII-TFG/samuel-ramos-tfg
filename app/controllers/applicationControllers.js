f1a3612fc13ef79b67e52d50515d449f896e9653
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-05-03 12:33:30 +0100

    Comentando el código

--- app/controllers/applicationControllers.js
+++ app/controllers/applicationControllers.js
@@ -1,25 +1,50 @@
 import Repo from '../models/repo'
 import Group from '../models/group'
 
-// Controller for get home page.
+/**
+* Funcion: home(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza la vista home
+*/
 function home (req, res) {
   res.render('static_pages/home', { titulo: 'CodeLab', usuario: req.user })
 }
 
-// Controller for logout.
+/**
+* Funcion: logout(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Controla el log out
+*/
 function logout (req, res) {
   req.logout()
   res.redirect('/')
 }
 
-// Controller for redirect home page.
+/**
+* Funcion: redirectHome(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Redirige a home
+*/
 function redirectHome (req, res) {
   res.redirect('/')
 }
 
-// Controller for get profile page.
+/**
+* Funcion: profiles(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza el perfil
+*/
 async function profiles (req, res) {
   try {
+    // Hacemos las consultas para saber que tareas ha realizado el alumno.
     let repos = await Repo.find({ 'StudentLogin': req.user.username })
     let groups = await Group.find({ })
     res.render('static_pages/profile', { titulo: 'Perfil', usuario: req.user, tareas: repos, tareasGrupales: groups })
@@ -28,11 +53,24 @@ async function profiles (req, res) {
   }
 }
 
-// Controller for get error page.
+/**
+* Funcion: err(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza la vista de error
+*/
 function err (req, res) {
   res.render('static_pages/error', { titulo: 'Error: Página no encontrada', usuario: req.user })
 }
 
+/**
+* Funcion: help(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza la vista de help
+*/
 function help (req, res) {
   res.render('static_pages/help', { titulo: 'Ayuda', usuario: req.user })
 }
--- app/controllers/assignControllers.js
+++ app/controllers/assignControllers.js
@@ -10,18 +10,29 @@ import Github from '../helpers/githubHelper'
 
 import * as Eval from '../helpers/evaluationHelper'
 
+// Expresión regular para el formato del nombre
 const nameFormat = new RegExp('[^a-zA-Z0-9_.-]+', 'g')
 
-// Controller for get the new assign page.
+/**
+* Funcion: newAssign(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza la vista de nueva asignación
+*/
 async function newAssign (req, res) {
   let orgLogin = req.params.idclass
 
   try {
+    // Solicitamos la organización
     let org = await Org.findOne({ 'login': orgLogin })
 
+    // Comprobamos que el dueño de la organización es el usuario activo.
     if (org.ownerLogin === req.user.username) {
+      // Si el dueño es el usuario activo le renderizamos la vista
       res.render('assignments/new', { titulo: 'Nueva tarea', usuario: req.user, classroom: orgLogin })
     } else {
+      // En caso contrario lo redirigimos a Home
       res.redirect('/classrooms')
     }
   } catch (error) {
@@ -29,7 +40,13 @@ async function newAssign (req, res) {
   }
 }
 
-// Controller for create the new assign in the db.
+/**
+* Funcion: newAssignP(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Crea la tarea en la base de datos
+*/
 async function newAssignP (req, res) {
   let orgLogin = req.params.idclass
   let titleFormated = req.body.titulo.replace(nameFormat, '-')
@@ -38,21 +55,26 @@ async function newAssignP (req, res) {
   console.log(req.body.admin)
 
   try {
+    // Hacemos una consulta de la organización
     let org = await Org.findOne({ 'login': orgLogin })
 
+    // Seteamos los permisos del usuario (admin o push)
     if (req.body.admin === 'true') {
       permisos = 'admin'
     } else {
       permisos = 'push'
     }
 
+    // Seteamos el tipo de repositorio (privado o público)
     if (req.body.repo === 'true') {
       repoType = true
     } else {
       repoType = false
     }
 
+    // Comprobamos que el dueño de la organización es el usuario activo.
     if (org.ownerLogin === req.user.username) {
+      // Creamos el objeto para insertarlo en Mongo
       let newAssign = new Assign({
         title: titleFormated,
         ownerLogin: req.user.username,
@@ -63,6 +85,7 @@ async function newAssignP (req, res) {
         isActive: true
       })
 
+      // Guardamos el objeto en la base de datos
       await newAssign.save()
 
       res.redirect('/classroom/' + orgLogin)
@@ -75,18 +98,29 @@ async function newAssignP (req, res) {
   }
 }
 
-// Controller for get the assign page.
+/**
+* Funcion: assign(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Reneriza la vista de la asignación
+*/
 async function assign (req, res) {
   let tarea = req.params.idassign
   let aula = req.params.idclass
   let titulo = 'Tarea ' + req.params.idassign
 
   try {
+    // Realizamos la consulta del aula.
     let org = await Org.findOne({ 'login': aula })
+    // Realizamos la consulta de las tareas del aula.
     let repos = await Repo.find({ 'orgLogin': aula, assignName: tarea })
+    // Consultamos los alumnos para mostrar su información asociada
     let alumnos = await Student.find({ 'orgName': aula })
 
+    // Comprobamos que la asignación existe en la organización.
     if (org) {
+      // Comprobamos que el usuario activo es el dueño
       if (org.ownerLogin === req.user.username) {
         res.render('assignments/assign', { titulo: titulo, usuario: req.user, assign: tarea, classroom: aula, assigns: repos, students: alumnos })
       } else {
@@ -100,7 +134,13 @@ async function assign (req, res) {
   }
 }
 
-// Controller for get the assign invitation page.
+/**
+* Funcion: assignInvi(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Reneriza la vista de la invitación a la asignación
+*/
 function assignInvi (req, res) {
   let tarea = req.params.idassign
   let aula = req.params.idclass
@@ -109,7 +149,13 @@ function assignInvi (req, res) {
   res.render('assignments/invitation', { titulo: titulo, usuario: req.user, assign: tarea, classroom: aula })
 }
 
-// Controller for  add a member to the individual assign.
+/**
+* Funcion: assignInviP(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Crea el repositorio y añade el repo a la base de datos
+*/
 async function assignInviP (req, res) {
   let tarea = req.params.idassign
   let aula = req.params.idclass
@@ -117,12 +163,16 @@ async function assignInviP (req, res) {
   let estudiante = req.user.username
 
   try {
+    // Se hacen la consulta de la asignación
     let assign = await Assign.findOne({ 'orgLogin': aula })
+    // Se buscan los datos del usuario que acepta
     let user = await User.findOne({ 'login': assign.ownerLogin })
 
+    // Se comprueba que la asignación está activa
     if (!assign.isActive) {
       res.render('static_pages/error', { titulo: 'Error', usuario: req.user, msg: 'Ya no puedes aceptar esta tarea, está deshabilitado.' })
     } else {
+      // Se crea el objeto del repositorio
       let newRepo = new Repo({
         name: repo,
         assignName: tarea,
@@ -130,12 +180,16 @@ async function assignInviP (req, res) {
         ownerLogin: user.login,
         orgLogin: aula
       })
+
+      // Se guarda el objeto en base de datos
       await newRepo.save()
 
+      // Se inicializa la github API
       const ghUser = new Github(user.token)
 
-      console.log(assign.userAdmin)
+      // Se crea el repositorio
       await ghUser.createRepo(aula, repo, 'Repo created by CodeLab', assign.repoType)
+      // Se añade al usuario como colaborador
       await ghUser.addCollaborator(aula, repo, estudiante, assign.userAdmin)
 
       res.redirect('https://github.com/' + aula + '/' + repo)
@@ -146,12 +200,19 @@ async function assignInviP (req, res) {
   }
 }
 
-// Controller for get the group invi page.
+/**
+* Funcion: groupInvi(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Reneriza la vista de la invitacion a la asignación grupal
+*/
 async function groupInvi (req, res) {
   let tarea = req.params.idassign
   let aula = req.params.idclass
   let titulo = 'Tarea ' + req.params.idassign
   try {
+    // Se buscan los equipos del aula
     let teams = await Team.find({ org: aula })
     res.render('assignments/groupInvi', { titulo: titulo, usuario: req.user, assign: tarea, classroom: aula, equipos: teams })
   } catch (error) {
@@ -159,7 +220,13 @@ async function groupInvi (req, res) {
   }
 }
 
-// Controller for  add a member to the group assign.
+/**
+* Funcion: groupInviP(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Gestiona la creación de equipos y tareas grupales
+*/
 async function groupInviP (req, res) {
   let aula = req.params.idclass
   let tarea = req.params.idassign
@@ -167,16 +234,23 @@ async function groupInviP (req, res) {
   let repo = tarea + '-' + teamName
 
   try {
+    // Se leen los datos del aula
     let assign = await Assign.findOne({ 'orgLogin': aula })
+    // Se leen los datos del usuario
     let user = await User.findOne({ 'login': assign.ownerLogin })
+    // Se leen los datos del equipo
     let team = await Team.findOne({ 'name': teamName })
 
+    // Se comprueba si la asignación está activa
     if (!assign.isActive) {
       res.render('static_pages/error', { titulo: 'Error', usuario: req.user, msg: 'Ya no puedes aceptar esta tarea, está deshabilitado.' })
     } else {
+      // Se actuliza el equipo con el miembro nuevo
       await Team.update({ name: team.name }, { $push: {members: req.user.username} })
 
+      // Se inicializa la Github API
       const ghUser = new Github(user.token)
+      // Se añade el miembro al equipo  en github
       await ghUser.addMember(team.id, req.user.username)
 
       res.redirect('https://github.com/' + aula + '/' + repo)
@@ -186,17 +260,27 @@ async function groupInviP (req, res) {
   }
 }
 
-// Controller for get the group assign page.
+/**
+* Funcion: groupAddign(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza la vista de la asignación grupal
+*/
 async function groupAssign (req, res) {
   let tarea = req.params.idassign
   let aula = req.params.idclass
   let titulo = 'Tarea ' + req.params.idassign
 
   try {
+    // Consulta de los datos de la organización
     let org = await Org.findOne({ 'login': aula })
+    // Consulta de las asignaciones grupales
     let repos = await Group.find({ 'orgLogin': aula, assignName: tarea })
 
+    // Se comprueba si la tarea existe en ese organización
     if (org) {
+      // Se comprueba si el usuario activo es el dueño
       if (org.ownerLogin === req.user.username) {
         res.render('assignments/groupAssign', { titulo: titulo, usuario: req.user, assign: tarea, classroom: aula, assigns: repos })
       } else {
@@ -210,7 +294,13 @@ async function groupAssign (req, res) {
   }
 }
 
-// Controller for get the newteams page
+/**
+* Funcion: team(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Renderiza la vista de creación de equipo
+*/
 function team (req, res) {
   let aula = req.params.idclass
   let tarea = req.params.idassign
@@ -219,7 +309,13 @@ function team (req, res) {
   res.render('assignments/newTeam', { titulo: titulo, usuario: req.user, classroom: aula, assign: tarea })
 }
 
-// Controller for create a new team and an new group assign
+/**
+* Funcion: teamP(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Crea un equipo nuevo
+*/
 async function teamP (req, res) {
   let aula = req.params.idclass
   let tarea = req.params.idassign
@@ -227,16 +323,22 @@ async function teamP (req, res) {
   let repo = tarea + '-' + teamFormated
   let idTeam
   try {
+    // Se consulta la información de la asignación
     let assign = await Assign.findOne({ 'orgLogin': aula })
+    // Se consulta la información del alumno
     let user = await User.findOne({ 'login': assign.ownerLogin })
 
+    // Se comprueba si la aginación está activa
     if (!assign.isActive) {
       res.render('static_pages/error', { titulo: 'Error', usuario: req.user, msg: 'Ya no puedes aceptar esta tarea, está deshabilitado.' })
     } else {
+      // Se inicializa la Github API
       const ghUser = new Github(user.token)
 
+      // Se crea el equipo en github
       let result = await ghUser.createTeam(aula, teamFormated, [req.user.username])
 
+      // Se crea el equipo
       let newTeam = new Team({
         name: teamFormated,
         id: result.data.id,
@@ -244,9 +346,12 @@ async function teamP (req, res) {
         org: aula
       })
 
+      // Se guarda el equipo en la base de datos
       await newTeam.save()
 
       idTeam = result.data.id
+
+      // Se crea el repositio grupal
       let newGroup = new Group({
         name: repo,
         assignName: tarea,
@@ -256,9 +361,12 @@ async function teamP (req, res) {
         orgLogin: aula
       })
 
+      // Se guarda el repositorio en la base de datos
       await newGroup.save()
 
+      // Se crea el equipo en la organización
       await ghUser.createRepo(aula, repo, 'Repo created by CodeLab', assign.assignType)
+      // Se añade el equipo al repositorio
       await ghUser.addTeam(idTeam, aula, repo)
       res.redirect('https://github.com/' + aula + '/' + repo)
     }
@@ -267,15 +375,24 @@ async function teamP (req, res) {
   }
 }
 
-// Controller for get the assign options
+/**
+* Funcion: optionsG(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Reneriza la vista de la asignación
+*/
 async function optionsG (req, res) {
   let aula = req.params.idclass
   let tarea = req.params.idassign
 
   try {
+    // Se consulta la información del aula
     let org = await Org.findOne({ 'login': aula })
+    // Se consulta la información de la organizacion
     let assign = await Assign.findOne({ 'orgLogin': aula, 'title': tarea })
 
+    // Se comprueba si el usuario activo es el dueño
     if (org.ownerLogin === req.user.username) {
       res.render('assignments/options', { titulo: 'Opciones', usuario: req.user, classroom: aula, assign: tarea, activado: assign.isActive })
     } else {
@@ -286,19 +403,37 @@ async function optionsG (req, res) {
   }
 }
 
-// Controller for save the options page
+/**
+* Funcion: optionsP(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Reneriza la vista de las opciones
+*/
 async function optionsP (req, res) {
   let aula = req.params.idclass
   let tarea = req.params.idassign
 
-  if (req.body.activador) {
-    await Assign.findOneAndUpdate({ titulo: tarea }, { isActive: true })
-  } else {
-    await Assign.findOneAndUpdate({ titulo: tarea }, { isActive: false })
+  try {
+    // Comprobamos si la asignación está activa o inactiva.
+    if (req.body.activador) {
+      await Assign.findOneAndUpdate({ titulo: tarea }, { isActive: true })
+    } else {
+      await Assign.findOneAndUpdate({ titulo: tarea }, { isActive: false })
+    }
+    res.redirect('/assign/' + aula + '/' + tarea)
+  } catch (error) {
+    console.log(error)
   }
-  res.redirect('/assign/' + aula + '/' + tarea)
 }
 
+/**
+* Funcion: evalRepo(req, res)
+*
+* Parámetros: req: request, res: response
+*
+* Descripción: Crea el repositorio de evaluación
+*/
 async function evalRepo (req, res) {
   let aula = req.params.idclass
   let tarea = req.params.idassign
@@ -306,9 +441,12 @@ async function evalRepo (req, res) {
   let readme = '# Eval repo\n' + 'Clone and exec ```./eval.sh``` for get all the students repos'
 
   try {
+    // Hacemos la llamada al create submodule
     let result = await Eval.createSubmodule(aula, tarea, req.user.username)
+    // Buscamos los datos del usuario
     let usr = await User.findOne({ 'login': req.user.username })
 
+    // Creamos el repositorio de evaluación
     await Eval.createEvalRepo(aula, evalRepo, usr, result, readme, res)
     res.redirect('/assign/' + tarea + '/' + tarea)
   } catch (error) {
